9e27bf61bb5a0f4a10116918b76647e9efe8cae6
Тимофей Кочетков <pochta30052001@gmail.com>
2023-05-14 20:09:46 +0400

    add deskr in readme

--- client-js-socket/check.js
+++ client-js-socket/check.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     let buttonCheck = document.querySelector("#check-quest");
 
-    let questId = 5;
+    let questId = 1;
     buttonCheck.addEventListener("click", async () => {
-        webSocket = new WebSocket(`ws://localhost:65456/check/${questId}`);
+        webSocket = new WebSocket(`ws://localhost:9007/check/${questId}`);
         webSocket.onopen = function (e) {
             webSocket.send(JSON.stringify("start"));
             console.log('start');
