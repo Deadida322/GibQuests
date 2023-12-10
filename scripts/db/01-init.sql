31042e09ef19eb21db0aa0fe51a1995e0e8e33ec
Тимофей Кочетков <pochta30052001@gmail.com>
2023-12-10 18:46:53 +0400

    fix gitignore py

--- scripts/db/01-init.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'CREATE DATABASE quests_sharp_auth' 
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'quests_sharp_auth')\gexec
-
-SELECT 'CREATE DATABASE quests_sharp_generate' 
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'quests_sharp_generate')\gexec
